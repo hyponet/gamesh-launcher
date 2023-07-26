ce702e4416250b5e6bf75fe7e9b9222bfc09ef94
hyponet <i@ihypo.net>
2023-07-26 13:41:08 +0000

    update menu
    Signed-off-by: hyponet <i@ihypo.net>

---
+++ Menu/GameShell/21_Zelda.sh
@@ -0,0 +1,1 @@
+retroarch -L /home/cpi/apps/emulators/mgba_libretro.so "/home/cpi/games/MGBA/塞尔达传说梦见岛DX.gbc"
--- Menu/GameShell/23_Zelda.sh
+++
@@ -1,1 +0,0 @@
-retroarch -L /home/cpi/apps/emulators/mgba_libretro.so "/home/cpi/games/MGBA/塞尔达传说_众神的三角力量+四支剑.gba"
